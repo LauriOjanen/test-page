4d0a73d087f7693d80673dfbba10c7e271af3655
LauriOjanen <78724537+LauriOjanen@users.noreply.github.com>
2023-05-18 08:54:10 +0300

    Home section added

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className='container-wrap'>
+    <div className='container'>
+        <h1>Jee jee</h1>
+    </div>
+    </div>
+  )
+}
+
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
+    <Home />
   </React.StrictMode>,
 )
